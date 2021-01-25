0c2f946484f2e8fd64ccfcc14275b21f666f9073
Diana Liao <dijali@gmail.com>
2021-01-25 06:03:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,13 +26,13 @@ end
 def get_english_meaning (filepath, j_icon)
   library = load_library(filepath)
   
-  if !library.has_value?(j_icon)
-    p "Sorry, that emoticon was not found"
-  else
+  #if !library.has_value?(j_icon)
+   # puts "Sorry, that emoticon was not found"
+  #else
     library.each do |meaning, guide|
       if guide.value? (j_icon)
        return meaning
       end 
     end 
-  end 
+  #end 
 end
