aeebf49356e7c6b622c07f7bda35603942ce52bf
Diana Liao <dijali@gmail.com>
2021-01-25 05:22:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(filepath)
   library = YAML.load_file(filepath)
   lang_keys = [:english, :japanese]
   library.each do |meaning, emoticons| 
-    meaning => []
+    meaning => [Hash.]
   end 
   binding.pry
   puts "whyyyy"
